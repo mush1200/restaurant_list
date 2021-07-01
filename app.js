95f5030f26bf7a4c7fb45efbcc37474b53ab3610
mush1200 <mush120@hotmail.com>
2021-07-01 15:10:19 +0800

    initialize Project

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
 
+//設定樣板引擎
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
 //設定路由
 app.get('/', (req, res) => {
-  res.send('hi')
+  res.render('index', { restaurants: restaurantList.results})
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', {restaurant: restaurant})
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: restaurants, keyword: keyword})
 })
 
 //啟動、設定監聽器
